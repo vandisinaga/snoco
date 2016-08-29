a361004b5d4e1a2eb17b0dd01978170709da0448
Vandi <vandi@upwardstech.com>
2016-08-30 01:41:31 +0700

    Update theme and homepage

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" class="search-form" action="http://snoco.s360.is/">
+    <label>
+        <span class="screen-reader-text">Search for:</span>
+        <input type="search" class="search-field"  value="" name="s">
+    </label>
+    <input type="submit" class="search-submit" value="Search">
+</form>
